b18858fd95118b7bed13325f3d3ddf9e7c28cd3d
João Guilherme Mendes <jmgvh9000@gmail.com>
2021-03-30 07:09:37 -0300

    .

--- javascript-download/JavaScript/Aula 12-exs02/script.js
+++ javascript-download/JavaScript/Aula 12-exs02/script.js
@@ -1,10 +1,13 @@
 function verificar() {
     var data = new Date()
     var ano = data.getUTCFullYear()
+
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
+
     if (fano.value.length == 0 || fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente ')
+        
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
